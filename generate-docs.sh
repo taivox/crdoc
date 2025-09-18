1c7ac27e43252ba8613de991b8ca37cae7a94884
taivox <taivox@gmail.com>
2025-09-18 21:42:53 +0300

    test

--- generate-docs.sh
+++ generate-docs.sh
@@ -2,7 +2,8 @@
 set -e
 
 sed -e 's/apiextensions\.crossplane\.io/apiextensions.k8s.io/g' \
-    -e 's/CompositeResourceDefinition/CustomResourceDefinition/g' ecr.yaml > ecr-modified.yaml
+    -e 's/CompositeResourceDefinition/CustomResourceDefinition/g' \
+    -e 's/kind: X\([A-Z][a-zA-Z0-9]*\)/kind: \1/g' ecr.yaml > ecr-modified.yaml
 
 go run . --resources ./ecr-modified.yaml --output output.md
 
